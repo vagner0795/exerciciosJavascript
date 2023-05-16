1d4d1acfca2937d526ebdf65d308e83270f04c5a
vagner0795 <vagner-se@hotmail.com>
2023-05-16 09:37:56 -0300

    feat(git): first commit javascript notes

---
+++ Modulo 1/aula 01.js
@@ -0,0 +1,23 @@
+// Lógica de Programação e Algoritimo na PRÁTICA
+
+// O que é logica de programação: forma de se comunicar com as
+// linguagens de programação. como dar ordens certas para que
+// a linguagem execute determinada ação. 
+//" Aprender como o computador pensa e como se comunicar com ele"
+
+// Algoritimo: sequência dos passos que serão seguidos para fazer alguma coisa
+// bem parecido com uma receita de bolo. tudo o que fazemos no dia a dia segue um 
+// passo a passo / um algoritimo.
+
+// EX
+
+// Algoritmo para fazer cafe:
+
+// SE a cafeteira estiver ligada na Tomada, seguir para o próximo passo.
+// SE NÂO, liga-la na Tomada.
+
+// SE o compartimento de água estiver cheio, seguir para o próximo passo.
+// SE NÂO, encha-o
+
+// SE tiver capsula de café, abrir e misturar com a água.
+// SE NÂO, adicionar capsula de café
