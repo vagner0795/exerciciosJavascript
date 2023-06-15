6f7628e4181a4cf3de5bd4890538a36194cbfcbe
vagner0795 <vagner-se@hotmail.com>
2023-06-15 10:51:13 -0300

    feat: add new lessons

---
+++ Modulo 3/aula03 - Operadores Aritimeticos/script.js
@@ -0,0 +1,38 @@
+//! Operadores Aritiméticos
+
+/*  
+
++ -> Adição
+- -> Subtração
+/ -> Divisão
+* -> Multiplicação
+% -> Resto
+++ -> Incremento
+-- -> Decremento
+** -> Exponencial
+
+
+*/  
+
+
+let result = 2 + 2 
+let result2 = 5 - 5
+let result3 = 80 * 4
+let result4 = 40 / 2
+let result5 = 50
+let result6 = 300 
+let result7 = 2 ** 3
+let result8 = 8 % 2
+
+result5++
+result6--
+
+
+console.log(result)
+console.log(result2)
+console.log(result3)
+console.log(result4)
+console.log(result5)
+console.log(result6)
+console.log(result7)
+console.log(result8)
---
+++ Modulo 3/aula04 - Operadores de Atribuição/script.js
@@ -0,0 +1,46 @@
+//! Operadores de Atribuição
+
+/*  
+
+ = -> Igualdade
+ += -> Adição
+ -= -> Subtração
+ /= -> Divisão
+ *= -> Multiplicação
+ %= -> Resto
+
+ */  
+
+ let result1 = 20
+
+ result1 += 70 //? result1 = result1 + 70
+
+ console.log(result1)
+
+ let result2 = 20
+
+ result2 -= 70 //? result2 = result2 - 70
+
+ console.log(result2)
+
+ let result3 = 100
+
+ result3 /= 70 //? result3 = result3 / 70
+
+ console.log(result3)
+
+ let result4 = 500
+
+ result4 *= 70 //? result4 = result4 * 70
+
+ console.log(result4)
+
+ let result5 = 2000
+
+ result5 %= 70 //? result5 = result5 % 70
+
+ console.log(result5)
+
+
+
+
